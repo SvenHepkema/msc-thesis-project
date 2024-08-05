28bb68cf721ad17e52528b0cacb2434dec6d2d78
SvenHepkema <47423359+SvenHepkema@users.noreply.github.com>
2024-08-05 17:06:15 +0200

    Added test all script

---
+++ test-all.sh
@@ -0,0 +1,3 @@
+LOG_FILE=/tmp/log
+parallel --joblog $LOG_FILE ./bin/executable 1024 {1} {2} {3} {4} 0 ::: 32 ::: bp ffor :::  0 1 ::: 0 1 2> /dev/null
+cat $LOG_FILE | tail -n +2 | perl -ane '$F[6] and print'

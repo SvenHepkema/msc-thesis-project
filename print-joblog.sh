034e05b07f9ed6941f1dea912ea56d5804ffc558
SvenHepkema <47423359+SvenHepkema@users.noreply.github.com>
2024-08-09 13:24:50 +0200

    Improved the testing script output with more verbose output

---
+++ print-joblog.sh
@@ -0,0 +1,34 @@
+LOG_FILE=/tmp/log
+
+echo "$(cat $LOG_FILE | head -n 1)"
+
+OUTPUT=$(cat $LOG_FILE | tail -n +2 | perl -ane 'print if $F[7] == 11' | sort)
+if [ -n "$OUTPUT" ]; then
+	echo Received SEGFAULT:
+	echo "$OUTPUT"
+fi
+
+OUTPUT=$(cat $LOG_FILE | tail -n +2 | perl -ane 'print if $F[7] == 6' | sort)
+if [ -n "$OUTPUT" ]; then
+	echo Received SIGABORT:
+	echo "$OUTPUT"
+fi
+
+OUTPUT=$(cat $LOG_FILE | tail -n +2 | perl -ane 'print if $F[7] != 0 and $F[7] != 6 and $F[7] != 11' | sort)
+if [ -n "$OUTPUT" ]; then
+	echo Received other signal:
+	echo "$OUTPUT"
+fi
+
+OUTPUT=$(cat $LOG_FILE | tail -n +2 | perl -ane '$F[6] and print' | sort)
+if [ -n "$OUTPUT" ]; then
+	echo Failed:
+	echo "$OUTPUT"
+fi
+
+OUTPUT=$(cat $LOG_FILE | tail -n +2 | perl -ane 'print if $F[6] == 0' | sort)
+if [ -n "$OUTPUT" ]; then
+	echo Success:
+	echo "$OUTPUT"
+fi
+
--- test-all.sh
+++ test-all.sh
@@ -1,3 +1,3 @@
 LOG_FILE=/tmp/log
 parallel --joblog $LOG_FILE ./bin/executable 1024 {1} {2} {3} {4} 0 ::: 8 16 32 64 ::: bp ffor :::  0 1 ::: 0 1 2> /dev/null
-cat $LOG_FILE | tail -n +2 | perl -ane '$F[6] and print'
+./print-joblog.sh

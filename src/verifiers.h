cb9fd001248aee90a0cdfcbea3021b571812e946
SvenHepkema <47423359+SvenHepkema@users.noreply.github.com>
2024-08-12 18:00:01 +0200

    Added azim ffor verifiers

--- src/verifiers.h
+++ src/verifiers.h
@@ -1,4 +1,5 @@
 #include "verification.h"
+#include <cstdint>
 
 namespace verifiers {
 
@@ -32,6 +33,44 @@ verify_azim_bitpacking(const size_t a_count, bool use_random_data) {
   auto decompress = [](const T *in, T *out,
                        const int32_t value_bit_width) -> void {
     azim::unpack(in, out, static_cast<uint8_t>(value_bit_width));
+  };
+
+  return verification::verify_all_value_bit_widths<T>(
+      a_count, verification::data::generate_bp_data<T>(use_random_data),
+      verification::apply_compression_to_all<T>(compress),
+      verification::apply_decompression_to_all<T>(decompress));
+}
+
+template <typename T>
+verification::VerificationResult<T>
+verify_bitpacking_against_azim(const size_t a_count, bool use_random_data) {
+  auto compress = [](const T *in, T *out,
+                     const int32_t value_bit_width) -> void {
+    cpu::bitpack<T>(in, out, static_cast<uint8_t>(value_bit_width));
+  };
+
+  auto decompress = [](const T *in, T *out,
+                       const int32_t value_bit_width) -> void {
+    azim::unpack(in, out, static_cast<uint8_t>(value_bit_width));
+  };
+
+  return verification::verify_all_value_bit_widths<T>(
+      a_count, verification::data::generate_bp_data<T>(use_random_data),
+      verification::apply_compression_to_all<T>(compress),
+      verification::apply_decompression_to_all<T>(decompress));
+}
+
+template <typename T>
+verification::VerificationResult<T>
+verify_bitunpacking_against_azim(const size_t a_count, bool use_random_data) {
+  auto compress = [](const T *in, T *out,
+                     const int32_t value_bit_width) -> void {
+    azim::pack(in, out, static_cast<uint8_t>(value_bit_width));
+  };
+
+  auto decompress = [](const T *in, T *out,
+                       const int32_t value_bit_width) -> void {
+    cpu::bitunpack<T>(in, out, static_cast<uint8_t>(value_bit_width));
   };
 
   return verification::verify_all_value_bit_widths<T>(
@@ -81,34 +120,128 @@ verification::VerificationResult<T> verify_ffor(const size_t a_count,
 }
 
 template <typename T>
+verification::VerificationResult<T> verify_azim_ffor(const size_t a_count,
+                                                     bool use_random_data) {
+  T temp_base = 125;
+  T *temp_base_p = &temp_base;
+
+  auto compress = [temp_base_p](const T *in, T *out,
+                                const int32_t value_bit_width) -> void {
+    azim::ffor(in, out, static_cast<uint8_t>(value_bit_width), temp_base_p);
+  };
+  auto decompress = [temp_base_p](const T *in, T *out,
+                                  const int32_t value_bit_width) -> void {
+    azim::unffor(in, out, static_cast<uint8_t>(value_bit_width), temp_base_p);
+  };
+
+  return verification::verify_all_value_bit_widths<T>(
+      a_count,
+      verification::data::generate_ffor_data<T>(use_random_data, temp_base),
+      verification::apply_compression_to_all<T>(compress),
+      verification::apply_decompression_to_all<T>(decompress));
+}
+
+template <typename T>
+verification::VerificationResult<T>
+verify_ffor_against_azim(const size_t a_count, bool use_random_data) {
+  T temp_base = 125;
+  T *temp_base_p = &temp_base;
+
+  auto compress = [temp_base_p](const T *in, T *out,
+                                const int32_t value_bit_width) -> void {
+    cpu::ffor(in, out, temp_base_p, value_bit_width);
+  };
+  auto decompress = [temp_base_p](const T *in, T *out,
+                                  const int32_t value_bit_width) -> void {
+    azim::unffor(in, out, static_cast<uint8_t>(value_bit_width), temp_base_p);
+  };
+
+  return verification::verify_all_value_bit_widths<T>(
+      a_count,
+      verification::data::generate_ffor_data<T>(use_random_data, temp_base),
+      verification::apply_compression_to_all<T>(compress),
+      verification::apply_decompression_to_all<T>(decompress));
+}
+
+template <typename T>
+verification::VerificationResult<T>
+verify_unffor_against_azim(const size_t a_count, bool use_random_data) {
+  T temp_base = 125;
+  T *temp_base_p = &temp_base;
+
+  auto compress = [temp_base_p](const T *in, T *out,
+                                const int32_t value_bit_width) -> void {
+    azim::ffor(in, out, static_cast<uint8_t>(value_bit_width), temp_base_p);
+  };
+  auto decompress = [temp_base_p](const T *in, T *out,
+                                  const int32_t value_bit_width) -> void {
+    cpu::unffor(in, out, temp_base_p, value_bit_width);
+  };
+
+  return verification::verify_all_value_bit_widths<T>(
+      a_count,
+      verification::data::generate_ffor_data<T>(use_random_data, temp_base),
+      verification::apply_compression_to_all<T>(compress),
+      verification::apply_decompression_to_all<T>(decompress));
+}
+
+template <typename T>
 std::function<verification::VerificationResult<T>(const size_t, const bool)>
 get_verifier(const std::string name) {
   if (name == "bp") {
     return
         [](const size_t count,
            const bool use_random_data) -> verification::VerificationResult<T> {
-          return verification::verifiers::verify_bitpacking<T>(count,
-                                                               use_random_data);
+          return verify_bitpacking<T>(count, use_random_data);
         };
-  } else if (name == "azim_bp") {
+  } else if (name == "azimbp") {
     return
         [](const size_t count,
            const bool use_random_data) -> verification::VerificationResult<T> {
-          return verification::verifiers::verify_azim_bitpacking<T>(
-              count, use_random_data);
+          return verify_azim_bitpacking<T>(count, use_random_data);
+        };
+  } else if (name == "bp_azimubp") {
+    return
+        [](const size_t count,
+           const bool use_random_data) -> verification::VerificationResult<T> {
+          return verify_bitpacking_against_azim<T>(count, use_random_data);
+        };
+  } else if (name == "azimbp_ubp") {
+    return
+        [](const size_t count,
+           const bool use_random_data) -> verification::VerificationResult<T> {
+          return verify_bitunpacking_against_azim<T>(count, use_random_data);
         };
   } else if (name == "gpu_bp") {
     return
         [](const size_t count,
            const bool use_random_data) -> verification::VerificationResult<T> {
-          return verification::verifiers::verify_gpu_bitpacking<T>(
-              count, use_random_data);
+          return verify_gpu_bitpacking<T>(count, use_random_data);
         };
   } else if (name == "ffor") {
-    return [](const size_t count, const bool use_random_data)
-               -> verification::VerificationResult<T> {
-      return verification::verifiers::verify_ffor<T>(count, use_random_data);
-    };
+    return
+        [](const size_t count,
+           const bool use_random_data) -> verification::VerificationResult<T> {
+          return verify_ffor<T>(count, use_random_data);
+        };
+  } else if (name == "azimffor") {
+    return
+        [](const size_t count,
+           const bool use_random_data) -> verification::VerificationResult<T> {
+          return verify_azim_ffor<T>(count, use_random_data);
+        };
+  } else if (name == "ffor_azimunffor") {
+    return
+        [](const size_t count,
+           const bool use_random_data) -> verification::VerificationResult<T> {
+          return verify_ffor_against_azim<T>(count, use_random_data);
+        };
+  } else if (name == "azimffor_unffor") {
+    return
+        [](const size_t count,
+           const bool use_random_data) -> verification::VerificationResult<T> {
+          return verify_unffor_against_azim<T>(count, use_random_data);
+        };
   } else {
     throw std::invalid_argument("This compression type is not supported");
   }
--- test-all.sh
+++ test-all.sh
@@ -1,4 +1,6 @@
-COMPRESSIONS_TO_VERIFY="bp ffor azim_bp gpu_bp"
+#COMPRESSIONS_TO_VERIFY="bp ffor azim_bp gpu_bp"
+#COMPRESSIONS_TO_VERIFY="bp_azimubp azimbp_ubp "
+COMPRESSIONS_TO_VERIFY="ffor azimffor ffor_azimunffor azimffor_unffor"
 LOG_FILE=/tmp/log
 parallel --joblog $LOG_FILE ./bin/executable 1024 {1} {2} {3} {4} 0 ::: 8 16 32 64 ::: $COMPRESSIONS_TO_VERIFY :::  0 1 2> /dev/null
 ./print-joblog.sh

0012c4cded81c1018c186cc22a7b8bbc60e3b800
SvenHepkema <47423359+SvenHepkema@users.noreply.github.com>
2024-10-08 18:20:54 +0200

    Added start from zero and boxplot on sets

--- data-analysis/graph-generator.py
+++ data-analysis/graph-generator.py
@@ -10,18 +10,21 @@ from pandas._libs.tslibs.fields import get_start_end_field
 
 import polars as pl
 import matplotlib.pyplot as plt
+import dataclasses
 
 
 class GraphTypes(enum.Enum):
     SCATTER_SPEED = 1
     SCATTER_AVG_SPEED = 2
     BOXPLOT_SPEED = 3
+    BOXPLOT_SETS_SPEED = 4
 
 
 GRAPH_TYPES_CLI_OPTIONS = {
     "scatter-speed": GraphTypes.SCATTER_SPEED,
     "scatter-average-speed": GraphTypes.SCATTER_AVG_SPEED,
     "boxplot-speed": GraphTypes.BOXPLOT_SPEED,
+    "boxplot-sets-speed": GraphTypes.BOXPLOT_SETS_SPEED,
 }
 
 DEFAULT_COLORS = [
@@ -31,6 +34,13 @@ DEFAULT_COLORS = [
     "tab:red",
     "tab:pink",
 ]
+
+
+@dataclasses.dataclass
+class GraphConfiguration:
+    start_from_zero: bool
+    show_legend: bool
+    output_name: str | None
 
 
 def return_default_x(df: pl.DataFrame) -> tuple[str, str]:
@@ -59,12 +69,12 @@ def output_graph(output_name: str | None = None):
         plt.savefig(output_name, dpi=1000, format="png", bbox_inches="tight")
 
 
-def convert_unique_str_to_colors(values: list[str])-> list[str]:
+def convert_unique_str_to_colors(values: list[str]) -> list[str]:
     unique = list(set(values))
     return [DEFAULT_COLORS[unique.index(x)] for x in values]
 
 
-def plot_scatter(results: pl.DataFrame, legend: bool, output_name: str | None = None):
+def plot_scatter(results: pl.DataFrame, config: GraphConfiguration):
     column_name, pretty_name = return_default_x(results)
     sets = get_sets(results)
     x = [results[column_name]]
@@ -76,12 +86,12 @@ def plot_scatter(results: pl.DataFrame, legend: bool, output_name: str | None = 
 
     plt.scatter(x, y, s=20, c=colors)
 
-    if legend:
+    if config.show_legend:
         plt.legend()
-    output_graph(output_name)
+    output_graph(config.output_name)
 
 
-def plot_scatter_average(results: pl.DataFrame, legend: bool, output_name: str | None = None):
+def plot_scatter_average(results: pl.DataFrame, config: GraphConfiguration):
     column_name, pretty_name = return_default_x(results)
 
     colors_index = 0
@@ -90,7 +100,9 @@ def plot_scatter_average(results: pl.DataFrame, legend: bool, output_name: str |
         x = []
         y = []
 
-        for _, function_results in set_results.group_by("function_id", maintain_order=True):
+        for _, function_results in set_results.group_by(
+            "function_id", maintain_order=True
+        ):
             x.append(function_results[column_name][0])
             y.append(function_results["execution_speed"].mean())
 
@@ -100,12 +112,12 @@ def plot_scatter_average(results: pl.DataFrame, legend: bool, output_name: str |
     plt.xlabel(pretty_name)
     plt.ylabel("Execution speed (us)")
 
-    if legend:
+    if config.show_legend:
         plt.legend()
-    output_graph(output_name)
+    output_graph(config.output_name)
 
 
-def plot_boxplot(results: pl.DataFrame, legend: bool, output_name: str | None = None):
+def plot_boxplot(results: pl.DataFrame, config: GraphConfiguration):
     column_name, pretty_name = return_default_x(results)
 
     assert len(get_sets(results)) == 1, "Boxplot only allows one set"
@@ -124,9 +136,29 @@ def plot_boxplot(results: pl.DataFrame, legend: bool, output_name: str | None = 
     ax.set_ylabel("Execution speed (us)")
     ax.set_xticklabels(x)
 
-    if legend:
-        plt.legend()
-    output_graph(output_name)
+    if config.start_from_zero:
+        ax.set_ylim(ymin=0)
+
+    output_graph(config.output_name)
+
+
+def plot_boxplot_sets(results: pl.DataFrame, config: GraphConfiguration):
+    x = []
+    y = []
+
+    for set_name, function_results in results.group_by("set_name", maintain_order=True):
+        x.append(set_name[0])
+        y.append([result / 1000 for result in function_results["execution_speed"]])
+
+    fig, ax = plt.subplots()
+    ax.boxplot(y)
+    ax.set_ylabel("Execution speed (us)")
+    ax.set_xticklabels(x)
+
+    if config.start_from_zero:
+        ax.set_ylim(ymin=0)
+
+    output_graph(config.output_name)
 
 
 def load_dataframes(filenames: str) -> pl.DataFrame:
@@ -142,15 +174,19 @@ def load_dataframes(filenames: str) -> pl.DataFrame:
 
 
 def main(args):
+    config = GraphConfiguration(
+        args.start_from_zero, args.show_legend, args.output_file_path
+    )
     df = load_dataframes(args.files)
 
     plot_options = {
         GraphTypes.SCATTER_SPEED: plot_scatter,
         GraphTypes.SCATTER_AVG_SPEED: plot_scatter_average,
         GraphTypes.BOXPLOT_SPEED: plot_boxplot,
+        GraphTypes.BOXPLOT_SETS_SPEED: plot_boxplot_sets,
     }
 
-    plot_options[args.plot](df, args.legend, args.output_file_path)
+    plot_options[args.plot](df, config)
 
 
 if __name__ == "__main__":
@@ -173,12 +209,20 @@ if __name__ == "__main__":
         help=f"Filename to save graph to",
     )
     parser.add_argument(
-        "-l",
-        "--legend",
+        "-sl",
+        "--show-legend",
         type=bool,
         default=False,
         action=argparse.BooleanOptionalAction,
         help="Adds a legend to the graph",
+    )
+    parser.add_argument(
+        "-sfz",
+        "--start-from-zero",
+        type=bool,
+        default=False,
+        action=argparse.BooleanOptionalAction,
+        help="Start the y axis from zero",
     )
 
     parser.add_argument(

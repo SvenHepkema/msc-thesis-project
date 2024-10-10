304c055aad506d301e35d1efc8ffcbdd3d50c216
SvenHepkema <47423359+SvenHepkema@users.noreply.github.com>
2024-10-10 14:42:56 +0200

    Readded benchmarkers.hpp

--- src/benchmark/benchmarkers.hpp
+++ src/benchmark/benchmarkers.hpp
@@ -10,8 +10,36 @@
 #include "../alp/alp-bindings.hpp"
 #include "../fls/compression.hpp"
 #include "../gpu-alp/alp-test-kernels-bindings.hpp"
+//#include "../gpu-fls/fls-benchmark-kernels-bindings.hpp"
 
 namespace benchmarkers {
+
+	/*
+template <typename T>
+verification::VerificationResult<T> bench_bp_contains_zero_value_bitwidths(
+    const size_t a_count, [[maybe_unused]] const std::string dataset_name) {
+  auto decompress_column_a = []([[maybe_unused]] const T *in, T *out,
+                                [[maybe_unused]] const int32_t value_bit_width,
+                                [[maybe_unused]] const size_t count) -> void {
+    out = 1;
+  };
+  auto decompress_column_b = [](const T *in, T *out,
+                                const int32_t value_bit_width,
+                                const size_t count) -> void {
+    fls::gpu::bench::query_bp_contains_zero(out, in, count, value_bit_width,
+                                            utils::get_values_per_lane<T>());
+  };
+
+  auto value_bit_widths =
+      verification::generate_integer_range<int32_t>(1, sizeof(T) * 8);
+
+  return verification::run_verifier_on_parameters<T, T, int32_t, int32_t>(
+      value_bit_widths, value_bit_widths, a_count, 1,
+      verification::get_equal_decompression_verifier<T, T, int32_t, int32_t>(
+          data::lambda::get_bp_zero_column<T>(), decompress_column_a,
+          decompress_column_b));
+}
+*/
 
 template <typename T>
 verification::VerificationResult<T>
@@ -31,8 +59,7 @@ bench_baseline(const size_t a_count, const std::string dataset_name) {
   };
 
   auto value_bit_widths =
-      verification::generate_integer_range<int32_t>(sizeof(T) *
-                                                                  8 / 2);
+      verification::generate_integer_range<int32_t>(sizeof(T) * 8 / 2);
 
   return verification::run_verifier_on_parameters<T, T, int32_t, int32_t>(
       value_bit_widths, value_bit_widths, a_count, a_count,
@@ -45,11 +72,10 @@ bench_baseline(const size_t a_count, const std::string dataset_name) {
 template <typename T>
 verification::VerificationResult<T>
 bench_gpu_alp(const size_t a_count, const std::string dataset_name) {
-  T value = 34.0;
   auto decompress_column_a =
-      [value](const alp::AlpCompressionData<T> *in, T *out,
-              [[maybe_unused]] const int32_t exception_percentage,
-              [[maybe_unused]] const size_t count) -> void {
+      [](const alp::AlpCompressionData<T> *in, T *out,
+         [[maybe_unused]] const int32_t exception_percentage,
+         [[maybe_unused]] const size_t count) -> void {
     /*
     T* data = new T[in->size];
 alp::int_decode<T>(data, in);
@@ -66,9 +92,9 @@ alp::int_decode<T>(data, in);
   };
 
   auto decompress_column_b =
-      [value](const alp::AlpCompressionData<T> *in, T *out,
-              [[maybe_unused]] const int32_t exception_percentage,
-              [[maybe_unused]] const size_t count) -> void {
+      [](const alp::AlpCompressionData<T> *in, T *out,
+         [[maybe_unused]] const int32_t exception_percentage,
+         [[maybe_unused]] const size_t count) -> void {
     // gpu::bench_alp_equal_to<T>(out, in, value);
     alp::gpu::test::decode_complete_alp_vector(out, in);
   };

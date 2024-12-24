66052cdd65cf0762ba71017250758d827b0ff9c1
SvenHepkema <47423359+SvenHepkema@users.noreply.github.com>
2024-12-24 12:25:16 +0100

    Added sanity check to collect traces

--- collect-traces.sh
+++ collect-traces.sh
@@ -1,4 +1,4 @@
-if [ "$#" -ne 5 ]; then
+if [ "$#" -lt 5 ]; then
     echo "Illegal number of parameters"
 		echo "Requires <function> <bitwidth> <count> <repeat> <output_file_name>"
 		exit 0
@@ -9,12 +9,22 @@ BITWIDTH=$2
 COUNT=$3
 REPEAT=$4
 OUTPUT_FILE=$5
+SKIP_SANITY=$6
 
 TMP_FILE=/tmp/results.txt
 rm $TMP_FILE
 touch $TMP_FILE
 
 echo ./bin/executable $FUNCTION $BITWIDTH random $COUNT 0 
+
+if [ "$#" -eq 5 ]; then
+	./bin/executable $FUNCTION $BITWIDTH random $COUNT 0 
+	if [ $? -ne 0 ]; then
+			echo "Did not exit with code 0."
+			exit
+	fi
+fi
+
 for i in $(seq 1 $REPEAT);
 do
 		echo "Starting run" $i

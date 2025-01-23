15b309c6ac26e799809c2582e2d6b7a108ac4864
SvenHepkema <47423359+SvenHepkema@users.noreply.github.com>
2025-01-23 17:17:50 +0100

    Refactored collect-traces.sh

--- collect-traces.sh
+++ collect-traces.sh
@@ -1,24 +1,16 @@
-if [ "$#" -lt 5 ]; then
-    echo "Illegal number of parameters"
-		echo "Requires <function> <bitwidth> <count> <repeat> <output_file_name>"
-		exit 0
-fi
-
-FUNCTION=$1
-BITWIDTH=$2
-COUNT=$3
-REPEAT=$4
-OUTPUT_FILE=$5
-SKIP_SANITY=$6
+OUTPUT_FILE=$1
+REPEAT=$2
+CORRECTNESS_TEST=$3
+COMMAND=${@:4}
 
 TMP_FILE=/tmp/results.txt
 rm $TMP_FILE
 touch $TMP_FILE
 
-echo ./bin/executable $FUNCTION $BITWIDTH random $COUNT 0 
+echo $COMMAND
 
-if [ "$#" -eq 5 ]; then
-	./bin/executable $FUNCTION $BITWIDTH random $COUNT 0 
+if [ "$CORRECTNESS_TEST" -eq 1 ]; then
+	$COMMAND
 	if [ $? -ne 0 ]; then
 			echo "Did not exit with code 0."
 			exit
@@ -28,7 +20,7 @@ fi
 for i in $(seq 1 $REPEAT);
 do
 		echo "Starting run" $i
-		nvprof --print-gpu-trace ./bin/executable $FUNCTION $BITWIDTH random $COUNT 0 2>&1 | grep void | awk '{print $2}' >> $TMP_FILE
+		nvprof --print-gpu-trace $COMMAND 2>&1 | grep void | awk '{print $2}' >> $TMP_FILE
 done
 
 ./data-analysis/results-reader.py $TMP_FILE $OUTPUT_FILE -r $REPEAT -v value-bit-width

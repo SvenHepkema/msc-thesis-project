779ce16560ec20ee981580a624130f4fda6846cb
SvenHepkema <47423359+SvenHepkema@users.noreply.github.com>
2025-01-23 16:24:38 +0100

    Updated test-fls.sh

--- test-fls.sh
+++ test-fls.sh
@@ -1,6 +1,6 @@
-COMPRESSIONS_TO_VERIFY="bp ffor gpu_bp_stateless gpu_bp_stateless_2vec gpu_bp_stateless_4vec gpu_bp_stateful gpu_unffor"
+KERNELS_TO_VERIFY="test_stateless_1_1 test_stateful_1_1 test_stateless_branchless_1_1 test_stateful_branchless_1_1"
 VECTOR_COUNT=1024
 
 LOG_FILE=/tmp/log
-parallel --joblog $LOG_FILE ./bin/executable {1} {2} {3} $VECTOR_COUNT 0 ::: $COMPRESSIONS_TO_VERIFY ::: 8 16 32 64 ::: index random 2> /dev/null
+parallel --joblog $LOG_FILE ./bin/executable verify_gpu_bp {1} {2} random vbw-0-{2} $VECTOR_COUNT 0 ::: $KERNELS_TO_VERIFY ::: 8 16 32 64 2> /dev/null
 ./print-joblog.sh

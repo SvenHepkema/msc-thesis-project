1461a5f21486ee31dae6586c60e699a837be69c4
SvenHepkema <47423359+SvenHepkema@users.noreply.github.com>
2025-01-24 16:51:38 +0100

    Updated the graph generator

--- data-analysis/graph-generator.py
+++ data-analysis/graph-generator.py
@@ -6,22 +6,24 @@ import sys
 import argparse
 import logging
 import enum
+from typing import NewType
+from io import StringIO
 
 import polars as pl
 import matplotlib.pyplot as plt
 
-plt.style.use('classic')
-
 
 class GraphTypes(enum.Enum):
-    SCATTER_SPEED = 1
+    SCATTER = 1
+    BAR = 2
 
 
 GRAPH_TYPES_CLI_OPTIONS = {
-    "scatter-speed": GraphTypes.SCATTER_SPEED,
+    "scatter": GraphTypes.SCATTER,
+    "bar": GraphTypes.BAR,
 }
 
-DEFAULT_COLORS = [
+COLOR_SET = [
     "tab:blue",
     "tab:green",
     "tab:orange",
@@ -45,25 +47,110 @@ LEGEND_POSITIONS = [
     "center",
 ]
 
+COLUMN_ALIASES_FOR_AXIS = {
+    "execution_time": "Median execution time (us)",
+    "ipc": "Instructions executed per cycle",
+    "inst_executed_global_loads": "Global loads executed",
+    "dram_read_bytes": "Total bytes read from DRAM to L2 cache",
+    "global_hit_rate": "L1 Cache global load hit rate",
+    "eligible_warps_per_cycle": "Average number of eligible warps / active cycle",
+}
+
+
+ColumnName = NewType("ColumnName", str)
+
+
 class GraphConfiguration:
+    x_axis_range: tuple[int | None, int | None]
     y_axis_range: tuple[int | None, int | None]
     show_legend: bool
     legend_position: str
     output_name: str | None
+    title: str | None
     h_line: int | None
 
     def __init__(self, args: argparse.Namespace) -> None:
+        self.x_axis_range = (0 if args.start_from_zero else None, None)
         self.y_axis_range = (0 if args.start_from_zero else None, args.y_axis_max_value)
         self.h_line = args.h_line
         self.show_legend = args.show_legend
         self.legend_position = args.legend_position
         self.output_name = args.output_file_path
+        self.title = args.title
+
+    def apply_to_ax(self, ax):
+        if self.h_line:
+            ax.axhline(
+                y=self.h_line / 1000, color="r", linestyle="--", label="Baseline"
+            )
+
+        #ax.set_xticks(range(0, 35, 5))  
+        #ax.set_xticks(range(32), minor=True)
+        #ax.tick_params(axis='x', which='minor', length=4, color='r', labelbottom=False)  # Minor ticks in red, no labels
+
+        ax.grid(which='both', linestyle='--', linewidth=0.08)
+        ax.set_xlim(self.x_axis_range)
+        ax.set_ylim(self.y_axis_range)
+
+        if self.show_legend:
+            ax.legend(fontsize=12, loc=self.legend_position.replace("-", " "))
 
 
-def return_default_x(df: pl.DataFrame) -> tuple[str, str]:
+
+    def output_graph(self):
+        plt.tight_layout()
+        if self.output_name is None:
+            plt.show()
+        elif self.output_name[-4:] == "eps":
+            plt.savefig(self.output_name, format="eps")
+        else:
+            plt.savefig(self.output_name, dpi=1000, format="png", bbox_inches="tight")
+
+
+class ResultsFile:
+    name: str
+    command: str
+    data: pl.DataFrame
+
+    def __init__(self, filename: str) -> None:
+        self.name = filename
+        with open(filename) as file:
+            lines = [line for line in file]
+            self.command = lines[0]
+            csv = "".join(lines[1:])
+            self.data = pl.read_csv(StringIO(csv))
+
+
+def load_files(file_names_arg: str) -> list[ResultsFile]:
+    return [ResultsFile(file_name) for file_name in file_names_arg.split(":")]
+
+
+def parse_column_names(
+    column_names_arg: str, files: list[ResultsFile]
+) -> list[ColumnName]:
+    inner_set_columns = set()
+
+    for file in files:
+        for column_name in file.data.columns:
+            inner_set_columns.add(column_name)
+
+    result = []
+    for column_name in column_names_arg.split(":"):
+        if column_name in inner_set_columns:
+            result.append(ColumnName(column_name))
+        else:
+            exit(
+                f"Specified column name was not found in every dataset: "
+                f"{column_name}, possible columnnames: {inner_set_columns}"
+            )
+
+    return result
+
+
+def get_default_x_axis_column(df: pl.DataFrame) -> tuple[str, str]:
     names = [
-        ("value_bit_width", "Value bit widths"),
-        ("exception_count", "Exception counts"),
+        ("vbw", "Value bit widths"),
+        ("ec", "Exception counts"),
     ]
 
     for column_name, pretty_name in names:
@@ -73,82 +160,64 @@ def return_default_x(df: pl.DataFrame) -> tuple[str, str]:
     return "id", "Function ID"
 
 
-def get_sets(df: pl.DataFrame) -> list[str]:
-    return list(df.unique(subset=["set_name"])["set_name"])
-
-
-def output_graph(output_name: str | None = None):
-    plt.tight_layout()
-    if output_name is None:
-        plt.show()
-    elif output_name[-4:] == "eps":
-        plt.savefig(output_name, format="eps")
-    else:
-        plt.savefig(output_name, dpi=1000, format="png", bbox_inches="tight")
-
-
-def convert_unique_str_to_colors(values: list[str]) -> list[str]:
-    unique = list(set(values))
-    return [DEFAULT_COLORS[unique.index(x)] for x in values]
-
-
-def plot_scatter(results: pl.DataFrame, config: GraphConfiguration):
-    column_name, pretty_name = return_default_x(results)
-
-    colors_index = 0
+def plot_scatter(
+    datasets: list[ResultsFile], columns: list[ColumnName], config: GraphConfiguration
+):
+    plt.style.use("classic")
     fig, ax = plt.subplots()
 
-    for name, set_results in results.group_by("set_name", maintain_order=True):
-        name = name[0]
-        x = []
-        y = []
+    # we do not support multiple column plotting (yet)
+    assert len(columns) == 1
+    column = columns[0]
 
-        for _, function_results in set_results.group_by(
-            "function_id", maintain_order=True
-        ):
-            x.append(function_results[column_name][0])
-            y.append(function_results["execution_speed"].mean() / 1000)
+    x_axis_column, pretty_x_axis_name = get_default_x_axis_column(datasets[0].data)
 
-        ax.scatter(x, y, s=20, linewidths=0, c=DEFAULT_COLORS[colors_index], label=name)
+    for i, dataset in enumerate(datasets):
+        x = dataset.data[x_axis_column]
+        y = dataset.data[column]
 
-        colors_index += 1
+        if columns[0] == "execution_time":
+            y = [value / 1000 for value in y]
 
-    if config.h_line:
-        ax.axhline(y=config.h_line / 1000, color="r", linestyle="--", label="Baseline")
-    ax.set_ylim(config.y_axis_range)
-
-    if config.show_legend:
-        ax.legend(fontsize=12, loc=config.legend_position.replace('-', ' '))
-
-    plt.xlabel(pretty_name)
-    plt.ylabel("Average execution speed (us)")
-
-    output_graph(config.output_name)
-
-
-def load_dataframes(filenames: str) -> pl.DataFrame:
-    files: list[str] = [filename.strip() for filename in args.files.split(":")]
-
-    results: list[pl.DataFrame] = []
-    for filename in files:
-        df = pl.read_csv(filename)
-        df = df.with_columns(
-            pl.lit(filename.split("/")[-1].split(".")[0]).alias("set_name")
+        ax.scatter(
+            x,
+            y,
+            s=24,
+            linewidths=0,
+            c=COLOR_SET[i],
+            label=dataset.name,
         )
-        results.append(df)
 
-    return pl.concat(results)
+    config.apply_to_ax(ax)
+
+    plt.xlabel(pretty_x_axis_name)
+    plt.ylabel(COLUMN_ALIASES_FOR_AXIS.get(column, column))
+
+    config.output_graph()
+
+
+def plot_bar(
+    data: list[ResultsFile], columns: list[ColumnName], config: GraphConfiguration
+):
+    pass
+
+
+PLOT_FUNCTION_MAPPING = {
+    GraphTypes.SCATTER: plot_scatter,
+    GraphTypes.BAR: plot_bar,
+}
 
 
 def main(args):
+    results = load_files(args.files)
+    assert len(results) > 0
+
+    columns = parse_column_names(args.columns, results)
+    assert len(columns) > 0
+
     config = GraphConfiguration(args)
-    df = load_dataframes(args.files)
 
-    plot_options = {
-        GraphTypes.SCATTER_SPEED: plot_scatter,
-    }
-
-    plot_options[args.plot](df, config)
+    PLOT_FUNCTION_MAPPING[args.plot](results, columns, config)
 
 
 if __name__ == "__main__":
@@ -164,17 +233,24 @@ if __name__ == "__main__":
         help=f"Specify how to plot the data.",
     )
     parser.add_argument(
+        "columns",
+        type=str,
+        help=f"Specify which column to plot.",
+    )
+    parser.add_argument(
         "-o",
         "--output-file-path",
         type=str,
         default=None,
         help=f"Filename to save graph to",
     )
+
+    # Graph styling
     parser.add_argument(
         "-sl",
         "--show-legend",
         type=bool,
-        default=False,
+        default=True,
         action=argparse.BooleanOptionalAction,
         help="Adds a legend to the graph",
     )
@@ -187,10 +263,17 @@ if __name__ == "__main__":
         help="Defines the position of the legend",
     )
     parser.add_argument(
+        "-t",
+        "--title",
+        type=str,
+        default=None,
+        help="Title of graph",
+    )
+    parser.add_argument(
         "-sfz",
         "--start-from-zero",
         type=bool,
-        default=False,
+        default=True,
         action=argparse.BooleanOptionalAction,
         help="Start the y axis from zero",
     )

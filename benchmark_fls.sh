560018f91aac6be11fd3063f812b742b353594e7
SvenHepkema <47423359+SvenHepkema@users.noreply.github.com>
2025-01-25 11:00:46 +0100

    Fixed output file of benchmarker to incorporate the experiment instead of only the kernel

--- benchmark_fls.sh
+++ benchmark_fls.sh
@@ -2,5 +2,5 @@ KERNELS_TO_VERIFY="stateless_1_1 stateful_1_1 stateless_branchless_1_1 stateful_
 EXPERIMENTS="fls_decompress fls_query"
 N_VEC=10000
 OUTPUT_DIR="data"
-parallel -j 1 ./collect-traces.py  -tr 3 -m global_hit_rate,dram_read_bytes,dram_write_bytes,ipc -o $OUTPUT_DIR/{1} -c \"./bin/executable {2} {1} 32 random vbw-0-32 $N_VEC 0\" ::: $KERNELS_TO_VERIFY ::: $EXPERIMENTS
+parallel -j 1 ./collect-traces.py  -tr 3 -m global_hit_rate,dram_read_bytes,dram_write_bytes,ipc -o $OUTPUT_DIR/{2}-{1} -c \"./bin/executable {2} {1} 32 random vbw-0-32 $N_VEC 0\" ::: $KERNELS_TO_VERIFY ::: $EXPERIMENTS
 ls $OUTPUT_DIR | parallel chown $(whoami) $OUTPUT_DIR/{}

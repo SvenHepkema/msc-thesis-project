8e453345b658153880a4f7cb8539bd509276e668
SvenHepkema <47423359+SvenHepkema@users.noreply.github.com>
2025-01-28 12:53:42 +0100

    Added compute to fls-benchmark runner

--- benchmark_fls.sh
+++ benchmark_fls.sh
@@ -1,5 +1,5 @@
 KERNELS_TO_VERIFY="stateless_1_1 stateful_1_1 stateless_branchless_1_1 stateful_branchless_1_1 stateless_4_1 stateful_4_1 stateless_branchless_4_1 stateful_branchless_4_1 stateless_1_4 stateful_1_4 stateless_branchless_1_4 stateful_branchless_1_4"
-EXPERIMENTS="fls_decompress fls_query"
+EXPERIMENTS="fls_decompress fls_query fls_compute"
 N_VEC=10000
 OUTPUT_DIR="data"
 METRICS="global_load_requests,global_hit_rate,l2_tex_hit_rate,inst_issued,stall_memory_dependency,stall_memory_throttle,dram_read_bytes,dram_write_bytes,ipc"

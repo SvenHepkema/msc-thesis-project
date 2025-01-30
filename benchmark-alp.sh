6e80d0ff528c3d6d9e2073a01a5b56dfe9dee48b
SvenHepkema <47423359+SvenHepkema@users.noreply.github.com>
2025-01-30 17:24:47 +0100

    Fixed benchmark scripts to not overwrite unpack vectors or unpack values

--- benchmark-alp.sh
+++ benchmark-alp.sh
@@ -3,11 +3,11 @@ PATCHERS_TO_VERIFY="stateless stateful naive naive_branchless prefetch_position 
 UNPACK_N_VECTORS="1 4"
 UNPACK_N_VALUES="1 4"
 DATATYPE_WIDTH=32
-EXPERIMENTS="alp_decompress alp_query"
+EXPERIMENTS="alp_query"
 N_VEC=10000
 OUTPUT_DIR="alp-data"
 METRICS="global_load_requests,global_hit_rate,l2_tex_hit_rate,inst_issued,stall_memory_dependency,stall_memory_throttle,dram_read_bytes,dram_write_bytes,ipc"
 
 mkdir $OUTPUT_DIR
-parallel --progress -j 1 ./collect-traces.py  -tr 3 -m $METRICS -o $OUTPUT_DIR/{2}-{1}-{5} -c \"./bin/executable {2} {1} {5} {3} {4} $DATATYPE_WIDTH random vbw-0-16 $N_VEC 0\" ::: $KERNELS_TO_VERIFY ::: $EXPERIMENTS ::: $UNPACK_N_VECTORS ::: $UNPACK_N_VALUES ::: $PATCHERS_TO_VERIFY
+parallel --progress -j 1 ./collect-traces.py  -tr 3 -m $METRICS -o $OUTPUT_DIR/{2}-{1}-{5}-{3}-{4} -c \"./bin/executable {2} {1} {5} {3} {4} $DATATYPE_WIDTH random vbw-0-16 $N_VEC 0\" ::: $KERNELS_TO_VERIFY ::: $EXPERIMENTS ::: $UNPACK_N_VECTORS ::: $UNPACK_N_VALUES ::: $PATCHERS_TO_VERIFY
 ls $OUTPUT_DIR | parallel chown $(whoami) $OUTPUT_DIR/{}
--- benchmark-fls.sh
+++ benchmark-fls.sh
@@ -8,5 +8,5 @@ OUTPUT_DIR="fls-data"
 METRICS="global_load_requests,global_hit_rate,l2_tex_hit_rate,inst_issued,stall_memory_dependency,stall_memory_throttle,dram_read_bytes,dram_write_bytes,ipc"
 
 mkdir $OUTPUT_DIR
-parallel --progress -j 1 ./collect-traces.py  -tr 3 -m $METRICS -o $OUTPUT_DIR/{2}-{1} -c \"./bin/executable {2} {1} none {3} {4} $DATATYPE_WIDTH random vbw-0-32 $N_VEC 0\" ::: $KERNELS_TO_VERIFY ::: $EXPERIMENTS ::: $UNPACK_N_VECTORS ::: $UNPACK_N_VALUES
+parallel --progress -j 1 ./collect-traces.py  -tr 3 -m $METRICS -o $OUTPUT_DIR/{2}-{1}-{3}-{4} -c \"./bin/executable {2} {1} none {3} {4} $DATATYPE_WIDTH random vbw-0-32 $N_VEC 0\" ::: $KERNELS_TO_VERIFY ::: $EXPERIMENTS ::: $UNPACK_N_VECTORS ::: $UNPACK_N_VALUES
 ls $OUTPUT_DIR | parallel chown $(whoami) $OUTPUT_DIR/{}

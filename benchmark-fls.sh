1c716e3203d21d20c44d56e4978f64a0e54c971d
SvenHepkema <47423359+SvenHepkema@users.noreply.github.com>
2025-01-30 16:39:22 +0100

    Created benchmark ALP

--- benchmark-fls.sh
+++ benchmark-fls.sh
@@ -4,7 +4,9 @@ UNPACK_N_VALUES="1"
 DATATYPE_WIDTH=32
 EXPERIMENTS="fls_decompress fls_query fls_compute"
 N_VEC=10000
-OUTPUT_DIR="data"
+OUTPUT_DIR="fls-data"
 METRICS="global_load_requests,global_hit_rate,l2_tex_hit_rate,inst_issued,stall_memory_dependency,stall_memory_throttle,dram_read_bytes,dram_write_bytes,ipc"
+
+mkdir $OUTPUT_DIR
 parallel --progress -j 1 ./collect-traces.py  -tr 3 -m $METRICS -o $OUTPUT_DIR/{2}-{1} -c \"./bin/executable {2} {1} none {3} {4} $DATATYPE_WIDTH random vbw-0-32 $N_VEC 0\" ::: $KERNELS_TO_VERIFY ::: $EXPERIMENTS ::: $UNPACK_N_VECTORS ::: $UNPACK_N_VALUES
 ls $OUTPUT_DIR | parallel chown $(whoami) $OUTPUT_DIR/{}

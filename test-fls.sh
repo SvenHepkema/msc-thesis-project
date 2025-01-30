0d115760a31973ec2abe2e732eb386f7657e26f1
SvenHepkema <47423359+SvenHepkema@users.noreply.github.com>
2025-01-30 14:07:08 +0100

    Added progress bar to test fls

--- test-fls.sh
+++ test-fls.sh
@@ -7,5 +7,5 @@ TYPES_TO_VERIFY="32"
 VECTOR_COUNT=1024
 
 LOG_FILE=/tmp/log
-parallel --joblog $LOG_FILE ./bin/executable {3} {1} none {4} {5} {2} random vbw-0-{2} $VECTOR_COUNT 0 ::: $KERNELS_TO_VERIFY ::: $TYPES_TO_VERIFY ::: $EXPERIMENTS_TO_VERIFY ::: $UNPACK_N_VECTORS ::: $UNPACK_N_VALUES 2> /dev/null
+parallel --progress --joblog $LOG_FILE ./bin/executable {3} {1} none {4} {5} {2} random vbw-0-{2} $VECTOR_COUNT 0 ::: $KERNELS_TO_VERIFY ::: $TYPES_TO_VERIFY ::: $EXPERIMENTS_TO_VERIFY ::: $UNPACK_N_VECTORS ::: $UNPACK_N_VALUES 
 ./print-joblog.sh

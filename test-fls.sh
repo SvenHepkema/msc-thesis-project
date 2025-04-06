3ec4a43e1feddc4a20b0bdd51eb1f1a8c2da3f8d
SvenHepkema <47423359+SvenHepkema@users.noreply.github.com>
2025-04-06 13:12:50 +0200

    Updated the test-fls.sh and test-alp.sh

--- test-fls.sh
+++ test-fls.sh
@@ -1,11 +1,10 @@
-KERNELS_TO_VERIFY="stateless stateful-cache stateful-local-1 stateful-local-2 stateful-local-4 stateful-register-1 stateful-register-2 stateful-register-4 stateful-register-branchless-1 stateful-register-branchless-2 stateful-register-branchless-4 stateless_branchless stateful_branchless"
-UNPACK_N_VECTORS="1 4"
-UNPACK_N_VALUES="1"
-EXPERIMENTS_TO_VERIFY="fls_decompress fls_query"
-#TYPES_TO_VERIFY="8 16 32 64"
-TYPES_TO_VERIFY="32 64"
-VECTOR_COUNT=1024
+TYPES="u32 u64"
+KERNELS="decompress query"
+UNPACK_N_VECS="1 4"
+UNPACK_N_VALS="1"
+UNPACKERS="stateless stateless-branchless stateful-cache stateful-local-1 stateful-local-2 stateful-local-4 stateful-register-1 stateful-register-2 stateful-register-4 stateful-register-branchless-1 stateful-register-branchless-2 stateful-register-branchless-4 stateful-branchless"
+VECTOR_COUNT=256
 
 LOG_FILE=/tmp/log
-parallel --progress --joblog $LOG_FILE ./bin/executable {3} {1} none {4} {5} {2} random vbw-0-{2} $VECTOR_COUNT 0 ::: $KERNELS_TO_VERIFY ::: $TYPES_TO_VERIFY ::: $EXPERIMENTS_TO_VERIFY ::: $UNPACK_N_VECTORS ::: $UNPACK_N_VALUES 
+parallel --progress --joblog $LOG_FILE ./bin/test {1} {2} {3} {4} {5} none 0 32 0 0 $VECTOR_COUNT 0 ::: $TYPES ::: $KERNELS ::: $UNPACK_N_VECS ::: $UNPACK_N_VALS ::: $UNPACKERS
 ./print-joblog.sh
